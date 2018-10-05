db3d2daa6897843400d49ab198f7bde2b3745c89
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-05 00:56:09 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@
 def display_rainbow(colors=[])
   rainbow = ""
   for x in colors do
-    rainbow += "#{colors[x][0].upcase}: #{colors[x]}"    
+    rainbow += "#{x[0].upcase}: #{x}"    
   end
   
   puts rainbow
