391036ea07579ed6df795262369a3f6355fb7678
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-05 01:00:00 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,9 +4,11 @@ def display_rainbow(colors=[])
   rainbow = ""
   for x in colors do
     rainbow += "#{x[0].upcase}: #{x}"
+    
     if x != colors.last do
       rainbow +=  ", "
     end
+    
   end
   
   puts rainbow
